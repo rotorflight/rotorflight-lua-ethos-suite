435fe370693b7ef5c0e4b373c327741c1a9f9ff4
Rob Thomson <rob@marotori.com>
2024-11-21 07:17:39 +0000

    Final cleanups

--- scripts/rfsuite/app/pages/drivetrain.lua
+++ scripts/rfsuite/app/pages/drivetrain.lua
@@ -1,10 +1,7 @@
 local labels = {}
 local fields = {}
 
-
-fields[#fields + 1] = {t = "0% Throttle PWM Value", help = "motorMinThrottle", min = 50, max = 2250, default = 1070, vals = {1,2}}
-fields[#fields + 1] = {t = "100% Throttle PWM value", help = "motorMaxThrottle", min = 50, max = 2250, default = 1070, vals = {3,4}}
-fields[#fields + 1] = {t = "Motor Stop PWM Value", help = "motorMinCommand", min = 50, max = 2250, default = 1070, vals = {5,6}}
+-- gear ratio and motor pole count
 
 labels[#labels + 1] = {t = "Main Motor Gear Ratio", label = 1, inline_size = 17.5}
 fields[#fields + 1] = {t = "Pinion", label = 1, inline=2, help = "motorGearRatioPinion", min = 0, max = 2000, default = 1, vals = {22,23}}
@@ -14,7 +11,14 @@ labels[#labels + 1] = {t = "Tail Motor Gear Ratio", label = 2, inline_size = 17.
 fields[#fields + 1] = {t = "Rear", label = 2, inline=2,  help = "motorGearRatioTailRear", min = 0, max = 2000, default = 1, vals = {26,27}}
 fields[#fields + 1] = {t = "Front", label = 2, inline=1,  help = "motorGearRatioTailFront", min = 0, max = 2000, default = 1, vals = {28,29}}
 
-fields[#fields + 1] = {t = "Motor Pole Count", help = "motorPollCount", min = 0, max = 256, default = 8, vals = {8}}
+fields[#fields + 1] = {t = "Motor Pole Count", help = "motorPollCount", min = 0, max = 256, default = 8, vals = {14}}
+
+-- question of if below params are put elsewhere?
+--fields[#fields + 1] = {t = "0% Throttle PWM Value", help = "motorMinThrottle", min = 50, max = 2250, default = 1070, vals = {1,2}}
+--fields[#fields + 1] = {t = "100% Throttle PWM value", help = "motorMaxThrottle", min = 50, max = 2250, default = 1070, vals = {3,4}}
+--fields[#fields + 1] = {t = "Motor Stop PWM Value", help = "motorMinCommand", min = 50, max = 2250, default = 1070, vals = {5,6}}
+
+
 
 local function postLoad(self)
     rfsuite.app.triggers.isReady = true
@@ -28,14 +32,14 @@ local function preSavePayload(payload)
     local newPayload = {}
 
     for i,v in ipairs(payload) do   
-            if i > 8 then
+            if i > 6 then
                     newPayload[i] = payload[i+1]
             else
                 newPayload[i] = payload[i]     
             end
             
     end
-    newPayload[13] = payload[8]
+
     
     return newPayload
 
