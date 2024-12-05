5f9e3996dd5a2b9c8e24c3cd1bde8628b1787826
Rob Thomson <rob@marotori.com>
2024-12-05 08:56:39 +0000

    Sbus Out Configs

--- scripts/rfsuite/app/pages/sbusout_tool.lua
+++ scripts/rfsuite/app/pages/sbusout_tool.lua
@@ -7,37 +7,65 @@ local idx = arg[1]
 
 local currentProfileChecked = false
 
+local minMaxIndex
+
 local ch = idx 
 local ch_str = "CH" .. tostring(ch + 1)
 local offset = 6 * ch -- 6 bytes per channel
 
 
+local servoCount = rfsuite.config.servoCount or 6
+local motorCount = 1
+if rfsuite.config.tailMode == 0 then
+    motorCount = 2
+end
+
 local minmax = {}
-minmax[0] = {min=1000,max=2000}     --Receiver
-minmax[1] = {min=-1000,max=1000}    --Mixer
-minmax[2] = {min=1000,max=2000}     --Servo
-minmax[3] = {min=0,max=1000}        --Motor
+minmax[0] = {min=1000,max=2000, sourceMax=24}     --Receiver
+minmax[1] = {min=-1000,max=1000,sourceMax=24}    --Mixer
+minmax[2] = {min=1000,max=2000, sourceMax=servoCount}     --Servo
+minmax[3] = {min=0,max=1000, sourceMax=motorCount}        --Motor
 
 local enableWakeup = false
+
 
 local function wakeup()
 
     if enableWakeup == true then
-
-        local value = rfsuite.app.Page.fields[1].value
-       
-            for i,v in ipairs(minmax) do
-                    if i == value then      
-                       -- rfsuite.app.formFields[2]:setMinimum(10)
-                       -- rfsuite.app.formFields[3]:setMaximum(20)
-                    end
-            end
+    
+        -- to avoid a page reload we contrain the field values using a wakeup call.
+        -- we could use postEdit on the fields line - but this does not update until 
+        -- you exit the field!
         
+       local currentMin = minmax[minMaxIndex].min
+       local currentMax = minmax[minMaxIndex].max
+       local currentSourceMax = minmax[minMaxIndex].sourceMax
+        
+       if rfsuite.app.Page.fields[2].value >= currentSourceMax then
+             rfsuite.app.Page.fields[2].value = currentSourceMax - 1
+       end   
+
+       if rfsuite.app.Page.fields[3].value <= currentMin then
+             rfsuite.app.Page.fields[3].value = currentMin
+       end
+       if rfsuite.app.Page.fields[4].value >= currentMax then
+             rfsuite.app.Page.fields[4].value = currentMax
+       end       
+        
+    
     end    
+end
+
+-- function to set min and max value based on index.
+local function setMinMaxIndex(self)
+     minMaxIndex = math.floor(rfsuite.app.Page.fields[1].value)     
 end
 
 
 local function postLoad(self)
+
+    setMinMaxIndex(self)
+
     rfsuite.app.triggers.isReady = true
     enableWakeup = true
 end
@@ -49,8 +77,8 @@ local function onNavMenu(self)
 
 end
 
-fields[#fields + 1] = {t = "Type", min = 0, max = 16, vals = {1 + offset}, table = {[0] = "Receiver", "Mixer", "Servo", "Motor"}}
-fields[#fields + 1] = {t = "Source Channel", min = 0, max = 15, offset = 1, vals = { 2 + offset}}
+fields[#fields + 1] = {t = "Type", min = 0, max = 16, vals = {1 + offset}, table = {[0] = "Receiver", "Mixer", "Servo", "Motor"},  postEdit = function(self) self.setMinMaxIndex(self, true) end}
+fields[#fields + 1] = {t = "Source", min = 0, max = 15, offset = 1, vals = { 2 + offset}}
 fields[#fields + 1] = {t = "Min", min = -2000, max = 2000, vals = {3 + offset,4 + offset}}
 fields[#fields + 1] = {t = "Max",  min = -2000, max = 2000, vals = {5 + offset,6 + offset}}
 
@@ -67,6 +95,7 @@ return {
     fields = fields,
     postLoad = postLoad,
     onNavMenu = onNavMenu, 
+    setMinMaxIndex = setMinMaxIndex,
     wakeup = wakeup,
     navButtons = {menu = true, save = true, reload = true, tool = false, help = true}    
 }
