4f89b75cda21980f83b0efb1e3aaa196d1060baf
Rob Thomson <rob@marotori.com>
2025-01-15 11:55:30 +0000

    Add heading

--- scripts/rfsuite/tasks/sensors/frsky.lua
+++ scripts/rfsuite/tasks/sensors/frsky.lua
@@ -53,6 +53,7 @@ createSensorList[0x5130] = {name = "PID Profile", unit = UNIT_RAW}
 createSensorList[0x5131] = {name = "Rates Profile", unit = UNIT_RAW}
 createSensorList[0x5110] = {name = "Adj Function", unit = UNIT_RAW}
 createSensorList[0x5111] = {name = "Adj Value", unit = UNIT_RAW}
+createSensorList[0x5210] = {name = "Heading", unit = UNIT_DEGREE}
 createSensorList[0x52F0] = {name = "Debug 0", unit = UNIT_RAW}
 createSensorList[0x52F1] = {name = "Debug 1", unit = UNIT_RAW}
 createSensorList[0x52F2] = {name = "Debug 2", unit = UNIT_RAW}
