74b4ddfbf37fec8450c0f3e24b8a8596ebda5d0c
Rob Thomson <rob@marotori.com>
2025-01-22 16:17:13 +0000

    Battery config

--- scripts/rfsuite/app/modules/battery/battery.lua
+++ scripts/rfsuite/app/modules/battery/battery.lua
@@ -1,54 +1,38 @@
 local labels = {}
 local fields = {}
 
-local gyroFilterType = {[0] = "NONE", "1ST", "2ND"}
 
-labels[#labels + 1] = {t = "Gyro lowpass 1", t2 = "Lowpass 1", label = "line1", inline_size = 40.15}
-fields[#fields + 1] = {t = "Filter type", label = "line1", inline = 1, min = 0, max = #gyroFilterType, vals = {2}, table = gyroFilterType}
 
-labels[#labels + 1] = {t = "", label = "line2", inline_size = 40.15}
-fields[#fields + 1] = {t = "Cutoff", help = "gyroLowpassFilterCutoff", label = "line2", inline = 1, min = 0, max = 4000, unit = "Hz", default = 100, vals = {3, 4}}
+fields[#fields + 1] = {t = "Max Cell Voltage", help = nil, min = 0, decimals=2, scale=100, max = 500, unit = "V", default = 4.3, vals = {8,9}}
+fields[#fields + 1] = {t = "Full Cell Voltage", help = nil, min = 0, decimals=2, scale=100, max = 500, unit = "V", default = 4.1, vals = {10,11}}
+fields[#fields + 1] = {t = "Warn Cell Voltage", help = nil, min = 0, decimals=2, scale=100, max = 500, unit = "V", default = 3.5, vals = {12,13}}
+fields[#fields + 1] = {t = "Min Cell Voltage", help = nil, min = 0, decimals=2, scale=100, max = 500, unit = "V", default = 3.3, vals = {6,7}}
+fields[#fields + 1] = {t = "Battery Capacity", help = nil, min = 0, max = 20000, step=50, unit = "mAh", default = 0, vals = {1, 2}}
+fields[#fields + 1] = {t = "Cell Count", help = nil, min = 0, max = 24, unit = nil, default = 6, vals = {3}}
 
-labels[#labels + 1] = {t = "Gyro lowpass 1 dynamic", t2 = "Lowpass 1 dyn.", label = "line3", inline_size = 40.15, type = 1}
-fields[#fields + 1] = {t = "Min cutoff", help = "gyroLowpassFilterDynamicCutoff", label = "line3", inline = 1, min = 0, max = 1000, unit = "Hz", vals = {16, 17}}
 
-labels[#labels + 1] = {t = "", label = "line4", inline_size = 40.15}
-fields[#fields + 1] = {t = "Max cutoff", help = "gyroLowpassFilterDynamicCutoff", label = "line4", inline = 1, min = 0, max = 1000, unit = "Hz", vals = {18, 19}}
 
-labels[#labels + 1] = {t = "Gyro lowpass 2", t2 = "Lowpass 2", label = "line5", inline_size = 40.15}
-fields[#fields + 1] = {t = "Filter type", label = "line5", inline = 1, min = 0, max = #gyroFilterType, vals = {5}, table = gyroFilterType}
 
-labels[#labels + 1] = {t = "", label = "line6", inline_size = 40.15}
-fields[#fields + 1] = {t = "Cutoff", help = "gyroLowpassFilterCutoff", label = "line6", inline = 1, min = 0, max = 4000, unit = "Hz", vals = {6, 7}}
 
-labels[#labels + 1] = {t = "Gyro notch 1", t2 = "Notch 1", label = "line7", inline_size = 13.6}
-fields[#fields + 1] = {t = "Center", help = "gyroLowpassFilterCenter", label = "line7", inline = 2, min = 0, max = 4000, unit = "Hz", vals = {8, 9}}
-fields[#fields + 1] = {t = "Cutoff", help = "gyroLowpassFilterCutoff", label = "line7", inline = 1, min = 0, max = 4000, unit = "Hz", vals = {10, 11}}
+-- Below are other fields on the same msp call.
+-- voltage meter source (val 4)
+-- current meter source (val 5)
+-- lvc perventage source (val 14)
+-- consumption warning percentage (vale 15)
 
-labels[#labels + 1] = {t = "Gyro notch 2", t2 = "Notch 2", label = "line9", inline_size = 13.6}
-fields[#fields + 1] = {t = "Center", help = "gyroLowpassFilterCenter", label = "line9", inline = 2, min = 0, max = 4000, unit = "Hz", vals = {12, 13}}
-fields[#fields + 1] = {t = "Cutoff", help = "gyroLowpassFilterCutoff", label = "line9", inline = 1, min = 0, max = 4000, unit = "Hz", vals = {14, 15}}
-
-labels[#labels + 1] = {t = "Dynamic Notch Filters", t2 = "Dyn. Notch Filters", label = "line11", inline_size = 13.6}
-fields[#fields + 1] = {t = "Count", help = "gyroDynamicNotchCount", label = "line11", inline = 2, min = 0, max = 8, default = 4, vals = {20}}
-fields[#fields + 1] = {t = "Q", help = "gyroDynamicNotchQ", label = "line11", inline = 1, min = 10, max = 100, default = 200, vals = {21}, decimals = 1, scale = 10}
-
-labels[#labels + 1] = {t = "", label = "line12", inline_size = 13.6}
-fields[#fields + 1] = {t = "Min", help = "gyroDynamicNotchMinHz", label = "line12", inline = 2, min = 10, max = 200, default = 25, unit = "Hz", vals = {22, 23}}
-fields[#fields + 1] = {t = "Max", help = "gyroDynamicNotchMaxHz", label = "line12", inline = 1, min = 100, max = 500, default = 245, unit = "Hz", vals = {24, 25}}
 
 local function postLoad(self)
     rfsuite.app.triggers.isReady = true
 end
 
 return {
-    read = 92, -- msp_FILTER_CONFIG
-    write = 93, -- msp_SET_FILTER_CONFIG
+    read = 32, -- MSP_BATTERY_CONFIG
+    write = 33, -- MSP_SET_BATTERY_CONFIG
     eepromWrite = true,
-    reboot = true,
-    title = "Filters",
-    minBytes = 25,
-    simulatorResponse = {0, 1, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 25, 25, 0, 245, 0},
+    reboot = false,
+    title = "Battery",
+    minBytes = 15,
+    simulatorResponse = {138, 2, 3, 1, 1, 74, 1, 174, 1, 154, 1, 94, 1, 100, 10},
     labels = labels,
     fields = fields,
     postLoad = postLoad
