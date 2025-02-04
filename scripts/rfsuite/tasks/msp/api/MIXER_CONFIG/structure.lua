d47e8619a2b4121bf57def8485f6d65574f7f53a
Rob Thomson <rob@marotori.com>
2025-02-04 17:44:41 +0000

    Fix struct

--- scripts/rfsuite/tasks/msp/api/MIXER_CONFIG/structure.lua
+++ scripts/rfsuite/tasks/msp/api/MIXER_CONFIG/structure.lua
@@ -24,13 +24,13 @@ local MSP_API_STRUCTURE = {{field = "main_rotor_dir", type = "U8"},
                            {field = "swash_ring", type = "U8"},
                            {field = "swash_phase", type = "U16"},
                            {field = "swash_pitch_limit", type = "U16"},
-                           {field = "swash_trim_0", type = "S16"},
-                           {field = "swash_trim_1", type = "S16"},
-                           {field = "swash_trim_2", type = "S16"},
+                           {field = "swash_trim_0", type = "U16"},
+                           {field = "swash_trim_1", type = "U16"},
+                           {field = "swash_trim_2", type = "U16"},
                            {field = "swash_tta_precomp", type = "U8"},
                            {field = "swash_geo_correction", type = "U8"},
-                           --{field = "collective_geo_correction_pos", type = "S8"},    -- something odd is going on here. 
-                           --{field = "collective_geo_correction_neg", type = "S8"}     -- these values are set; but dont seem to actually get sent in the msp
+                           {field = "collective_geo_correction_pos", type = "S8"},    -- something odd is going on here. 
+                           {field = "collective_geo_correction_neg", type = "S8"}     -- these values are set; but dont seem to actually get sent in the msp
                         }
 
 -- Export the shared MSP API structure
