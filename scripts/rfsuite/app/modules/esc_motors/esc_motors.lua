835ecfd538fdfe347dc7e7d4db113d9d72556714
Rob Thomson <rob@marotori.com>
2025-02-17 21:49:59 +0000

    Update esc_motors.lua

--- scripts/rfsuite/app/modules/esc_motors/esc_motors.lua
+++ scripts/rfsuite/app/modules/esc_motors/esc_motors.lua
@@ -15,21 +15,21 @@ local apiform = {
         }
         },
         fields = {
-            {t = "Pinion",                          label = 1, inline = 2, mspapi = 1, apikey = "main_rotor_gear_ratio_0"},
-            {t = "Main",                            label = 1, inline = 1, mspapi = 1, apikey = "main_rotor_gear_ratio_1"},
-            {t = "Rear",                            label = 2, inline = 2, mspapi = 1, apikey = "tail_rotor_gear_ratio_0"},
-            {t = "Front",                           label = 2, inline = 1, mspapi = 1, apikey = "tail_rotor_gear_ratio_1"},
-            {t = "Motor Pole Count",                                       mspapi = 1, apikey = "motor_pole_count_0"},
-            {t = "0% Throttle PWM Value",                                  mspapi = 1, apikey = "minthrottle"},
-            {t = "100% Throttle PWM value",                                mspapi = 1, apikey = "maxthrottle"},
-            {t = "Motor Stop PWM Value",                                   mspapi = 1, apikey = "mincommand"},
-            
-            {t = "Protocol",                                               mspapi = 2, apikey = "protocol", type = 1, label = 3, inline = 2},
-            {t = "Pin Swap",                                               mspapi = 2, apikey = "pin_swap", type = 1, label = 3, inline = 1},
-            {t = "Half Duplex",                                            mspapi = 2, apikey = "half_duplex", type = 1, label = 4, inline = 2},
-            {t = "Update HZ",                                              mspapi = 2, apikey = "update_hz", label = 4, inline = 1},
-            {t = "Current Correction Factor",                              mspapi = 2, apikey = "current_correction_factor", apiversion = 12.08},
-            {t = "Consumption Correction Factor",                          mspapi = 2, apikey = "consumption_correction_factor", apiversion = 12.08}
+            {t = "Pinion",                          mspapi = 1, apikey = "main_rotor_gear_ratio_0",       label = 1, inline = 2},
+            {t = "Main",                            mspapi = 1, apikey = "main_rotor_gear_ratio_1",       label = 1, inline = 1},
+            {t = "Rear",                            mspapi = 1, apikey = "tail_rotor_gear_ratio_0",       label = 2, inline = 2},
+            {t = "Front",                           mspapi = 1, apikey = "tail_rotor_gear_ratio_1",       label = 2, inline = 1},
+            {t = "Motor Pole Count",                mspapi = 1, apikey = "motor_pole_count_0"},
+            {t = "0% Throttle PWM Value",           mspapi = 1, apikey = "minthrottle"},
+            {t = "100% Throttle PWM value",         mspapi = 1, apikey = "maxthrottle"},
+            {t = "Motor Stop PWM Value",            mspapi = 1, apikey = "mincommand"},
+
+            {t = "Protocol",                        mspapi = 2, apikey = "protocol",                      type = 1, label = 3, inline = 2},
+            {t = "Pin Swap",                        mspapi = 2, apikey = "pin_swap",                      type = 1, label = 3, inline = 1},
+            {t = "Half Duplex",                     mspapi = 2, apikey = "half_duplex",                   type = 1, label = 4, inline = 2},
+            {t = "Update HZ",                       mspapi = 2, apikey = "update_hz",                     label = 4, inline = 1},
+            {t = "Current Correction Factor",       mspapi = 2, apikey = "current_correction_factor",     apiversion = 12.08},
+            {t = "Consumption Correction Factor",   mspapi = 2, apikey = "consumption_correction_factor", apiversion = 12.08}
         }
     }                 
 }
