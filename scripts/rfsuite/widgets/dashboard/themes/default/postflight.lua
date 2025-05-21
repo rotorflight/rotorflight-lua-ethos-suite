a2c64e32569dc7988bbf9e8b5fc223fb5e981129
Rob Thomson <rob@marotori.com>
2025-05-21 15:50:38 +0100

    Code tidy up

--- scripts/rfsuite/widgets/dashboard/themes/default/postflight.lua
+++ scripts/rfsuite/widgets/dashboard/themes/default/postflight.lua
@@ -15,24 +15,6 @@
  * Note: Some icons have been sourced from https://www.flaticon.com/
 ]]--
 
---[[
-Example dashboard options:
-
-return {
-    layout = {
-        cols = 3,
-        rows = 4,
-        padding = 4
-    },
-    boxes = {
-        {col=1, row=1, type="telemetry", source="governor", title="GOVERNOR", unit="", color=nil, titlealign="left", titlecolor="red", valuealign="right", titlepaddingleft=10, transform=function(v) return rfsuite.utils.getGovernorState(v) end},
-        {col=2, row=1, type="telemetry", source="current", title="CURRENT", unit="A", color="orange", transform=function(v) return v and math.floor(v / 100) * 100 end, titlealign="center", valuealign="center"},
-        {col=3, row=1, type="telemetry", source="rpm", title="RPM", unit="RPM", color="red", transform="floor", titlealign="right", valuealign="left"},
-        {col=1, row=2, colspan=2, type="text", value="PRE-FLIGHT", title="", unit="", color={0,188,4}, bgcolor="blue", titlealign="center", valuealign="center"},
-        {col=3, row=2, type="image", value="widgets/dashboard/default_image.png", title="My Craft", titlecolor="black", bgcolor="white", imagepadding=10, imagewidth=60, imageheight=60, imagealign="left", titlepos="bottom"}
-    }
-}
-]]
 
 local telemetry = rfsuite.tasks.telemetry
 
