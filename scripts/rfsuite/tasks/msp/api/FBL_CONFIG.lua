7112ce2dd443e20dd8fa926362cda0cdb445c2ce
Rob Thomson <rob@marotori.com>
2025-07-31 07:53:40 +0100

    Add api file

---
+++ scripts/rfsuite/tasks/msp/api/FBL_CONFIG.lua
@@ -0,0 +1,186 @@
+--[[
+ * Copyright (C) Rotorflight Project
+ *
+ * License GPLv3: https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Note. Some icons have been sourced from https://www.flaticon.com/
+]] --
+-- Constants for MSP Commands
+local API_NAME = "FBL_CONFIG" -- API name (must be same as filename)
+local MSP_API_CMD_READ = 16 -- Command identifier for MSP Mixer Config Read
+
+
+-- Define the MSP response data structures
+local MSP_API_STRUCTURE_READ_DATA = {
+    {field = "version_major",     type = "U8"},                                         -- Major version
+    {field = "version_minor",     type = "U8"},                                         -- Minor version
+    {field = "version_patch",     type = "U8"},                                         -- Patch version
+    {field = "U_ID_0",            type = "U32",   simResponse = {43, 0, 34, 0}},        -- Unique ID 0
+    {field = "U_ID_1",            type = "U32",   simResponse = {9, 81, 51, 52}},       -- Unique ID 1
+    {field = "U_ID_2",            type = "U32",   simResponse = {52, 56, 53, 49}},      -- Unique ID 2
+    {field = "name",              type = "U128"},                                       -- Name
+    {field = "gov_mode",          type = "U8",    simResponse = {3}},
+    {field = "aileron",           type = "U8",    simResponse = {0}, min = 0, max = 15, default = 0},
+    {field = "elevator",          type = "U8",    simResponse = {1}, min = 0, max = 15, default = 1},
+    {field = "rudder",            type = "U8",    simResponse = {2}, min = 0, max = 15, default = 2},
+    {field = "collective",        type = "U8",    simResponse = {3}, min = 0, max = 15, default = 3},
+    {field = "throttle",          type = "U8",    simResponse = {4}, min = 0, max = 15, default = 4},
+    {field = "aux1",              type = "U8",    simResponse = {5}, min = 0, max = 15, default = 5},
+    {field = "aux2",              type = "U8",    simResponse = {6}, min = 0, max = 15, default = 6},
+    {field = "aux3",              type = "U8",    simResponse = {7}, min = 0, max = 15, default = 7},
+    {field = "servo_count",       type = "U8",    simResponse = {4}},
+    {field = "tail_rotor_mode",   type = "U8",    apiVersion = 12.06, simResponse = {0}},
+    {field = "swash_type",        type = "U8",    apiVersion = 12.06, simResponse = {0}},
+}
+
+-- Process structure in one pass
+local MSP_API_STRUCTURE_READ, MSP_MIN_BYTES, MSP_API_SIMULATOR_RESPONSE =
+    rfsuite.tasks.msp.api.prepareStructureData(MSP_API_STRUCTURE_READ_DATA)
+
+-- Variable to store parsed MSP data
+local mspData = nil
+local mspWriteComplete = false
+local payloadData = {}
+local defaultData = {}
+
+-- Create a new instance
+local handlers = rfsuite.tasks.msp.api.createHandlers()
+
+-- Variables to store optional the UUID and timeout for payload
+local MSP_API_UUID
+local MSP_API_MSG_TIMEOUT
+
+-- Function to initiate MSP read operation
+local function read()
+    if MSP_API_CMD_READ == nil then
+        rfsuite.utils.log("No value set for MSP_API_CMD_READ", "debug")
+        return
+    end
+
+    local message = {
+        command = MSP_API_CMD_READ,
+        processReply = function(self, buf)
+            local structure = MSP_API_STRUCTURE_READ
+            rfsuite.tasks.msp.api.parseMSPData(buf, structure, nil, nil, function(result)
+                mspData = result
+                if #buf >= MSP_MIN_BYTES then
+                    local completeHandler = handlers.getCompleteHandler()
+                    if completeHandler then completeHandler(self, buf) end
+                end
+            end)
+        end,
+        errorHandler = function(self, buf)
+            local errorHandler = handlers.getErrorHandler()
+            if errorHandler then errorHandler(self, buf) end
+        end,
+        simulatorResponse = MSP_API_SIMULATOR_RESPONSE,
+        uuid = MSP_API_UUID,
+        timeout = MSP_API_MSG_TIMEOUT  
+    }
+    rfsuite.tasks.msp.mspQueue:add(message)
+end
+
+local function write(suppliedPayload)
+    return nil -- No write operation defined for this API
+end
+
+-- Function to get the value of a specific field from MSP data
+local function readValue(fieldName)
+    if mspData and mspData['parsed'][fieldName] ~= nil then return mspData['parsed'][fieldName] end
+    return nil
+end
+
+-- Function to set a value dynamically
+local function setValue(fieldName, value)
+    payloadData[fieldName] = value
+end
+
+-- Function to check if the read operation is complete
+local function readComplete()
+    return mspData ~= nil and #mspData['buffer'] >= MSP_MIN_BYTES
+end
+
+-- Function to check if the write operation is complete
+local function writeComplete()
+    return mspWriteComplete
+end
+
+-- Function to reset the write completion status
+local function resetWriteStatus()
+    mspWriteComplete = false
+end
+
+-- Function to return the parsed MSP data
+local function data()
+    return mspData
+end
+
+-- set the UUID for the payload
+local function setUUID(uuid)
+    MSP_API_UUID = uuid
+end
+
+-- set the timeout for the payload
+local function setTimeout(timeout)
+    MSP_API_MSG_TIMEOUT = timeout
+end
+
+-- Function to get the API version in major.minor format
+local function readVersion()
+    if mspData then
+        local parsed = mspData['parsed']
+        return string.format("%d.%d.%d", parsed.version_major, parsed.version_minor, parsed.version_patch)
+    end
+    return nil
+end
+
+
+local function readRfVersion()
+
+  local MAJOR_OFFSET = 2
+  local MINOR_OFFSET = 3
+
+
+  local raw = readVersion()
+  if not raw then return nil end
+
+  -- split into numbers
+  local maj, min, patch = raw:match("(%d+)%.(%d+)%.(%d+)")
+  maj = tonumber(maj) - MAJOR_OFFSET
+  min = tonumber(min) - MINOR_OFFSET
+  patch = tonumber(patch)
+
+  -- guard against negatives or parse errors
+  if maj < 0 or min < 0 then
+    return raw
+  end
+
+  return string.format("%d.%d.%d", maj, min, patch)
+end
+
+-- Return the module's API functions
+return {
+    read = read,
+    write = write,
+    readComplete = readComplete,
+    writeComplete = writeComplete,
+    readValue = readValue,
+    setValue = setValue,
+    resetWriteStatus = resetWriteStatus,
+    setCompleteHandler = handlers.setCompleteHandler,
+    setErrorHandler = handlers.setErrorHandler,
+    data = data,
+    setUUID = setUUID,
+    setTimeout = setTimeout,
+    readVersion = readVersion,
+    readRfVersion = readRfVersion
+}
