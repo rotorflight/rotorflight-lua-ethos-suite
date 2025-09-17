dd957acbac5f5af5c99aa5d4dd022053cf0523cf
Rob Thomson <rob@marotori.com>
2025-09-17 23:39:00 +0100

    evect if less than 500

--- scripts/rfsuite/lib/compile.lua
+++ scripts/rfsuite/lib/compile.lua
@@ -55,7 +55,7 @@ local function cachename(name)
 end
 
 -- Adaptive LRU Cache with Pinning Support
-local LUA_RAM_THRESHOLD = 32 * 1024
+local LUA_RAM_THRESHOLD = 500 * 1024 -- 500 KB
 local LRU_HARD_LIMIT    = 10
 local EVICT_INTERVAL    = 5
 
