b255e0216aa426dccca63666965180ffe06649b2
Rob Thomson <rob@marotori.com>
2025-09-30 06:55:02 +0100

    Revert "Fix retry count var being declared after use (#1443)"
    This reverts commit 83e2cfdc27ec9483dc1be39e53456c5ae49102e1.

--- scripts/rfsuite/app/lib/ui.lua
+++ scripts/rfsuite/app/lib/ui.lua
@@ -1932,8 +1932,6 @@ function ui.requestPage()
       return
     end
 
-    local retryCount = app.Page.apidata.retryCount[apiKey] or 0   
-
     local v      = apiList[state.currentIndex]
     local apiKey = type(v) == "string" and v or v.name
     if not apiKey then
@@ -1950,6 +1948,7 @@ function ui.requestPage()
 
     if app and app.Page and app.Page.apidata then app.Page.apidata.retryCount = app.Page.apidata.retryCount or {} end
 
+    local retryCount = app.Page.apidata.retryCount[apiKey] or 0
     local handled = false
 
     log("[PROCESS] API: " .. apiKey .. " (Attempt " .. (retryCount + 1) .. ")", "debug")
