21e6cdfa5b997ff9ed70171198f5f59f9c56b8e5
Rob Thomson <rob@marotori.com>
2025-11-12 07:43:30 +0000

    Add global requite

--- scripts/rfsuite/tasks/sensors/frsky.lua
+++ scripts/rfsuite/tasks/sensors/frsky.lua
@@ -2,6 +2,7 @@
   Copyright (C) 2025 Rotorflight Project
   GPLv3 — https://www.gnu.org/licenses/gpl-3.0.en.html
 ]] --
+ local rfsuite = require("rfsuite")
 
 local arg = {...}
 local config = arg[1]
