9bab812f2f4ac74af7594950590c7aab4e2131c9
Rob Thomson <rob@marotori.com>
2026-02-14 08:53:08 +0000

    Update init.lua

--- src/rfsuite/app/modules/adjustments/init.lua
+++ src/rfsuite/app/modules/adjustments/init.lua
@@ -3,6 +3,6 @@
   GPLv3 -- https://www.gnu.org/licenses/gpl-3.0.en.html
 ]] --
 
-local init = {title = "Adjustments", section = "advanced", script = "adjustments.lua", image = "adjustments.png", order = 10, ethosversion = {1, 6, 2}}
+local init = {title = "Adjustments", section = "advanced", script = "adjustments.lua", image = "adjustments.png", order = 10, ethosversion = {1, 6, 2}, loaderspeed = 0.1}
 
 return init
