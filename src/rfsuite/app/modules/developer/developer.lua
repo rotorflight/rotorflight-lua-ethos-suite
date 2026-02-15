b26b15350825afd886c55b6872a7e17a9afd5dbd
Rob Thomson <rob@marotori.com>
2026-02-15 08:02:23 +0000

    move

--- src/rfsuite/app/modules/developer/developer.lua
+++ src/rfsuite/app/modules/developer/developer.lua
@@ -12,7 +12,8 @@ local TITLE = "Developer"
 local S_PAGES = {
     {name = "@i18n(app.modules.msp_speed.name)@", script = "developer/tools/msp_speed.lua", image = "app/modules/developer/gfx/msp_speed.png", bgtask = true, offline = true},
     {name = "@i18n(app.modules.api_tester.name)@", script = "developer/tools/api_tester.lua", image = "app/modules/developer/gfx/api_tester.png", bgtask = true, offline = true},
-    {name = "@i18n(app.modules.msp_exp.name)@", script = "developer/tools/msp_exp.lua", image = "app/modules/developer/gfx/msp_exp.png", bgtask = true, offline = true}
+    {name = "@i18n(app.modules.msp_exp.name)@", script = "developer/tools/msp_exp.lua", image = "app/modules/developer/gfx/msp_exp.png", bgtask = true, offline = true},
+    {name = "@i18n(app.modules.settings.name)@", script = "settings/tools/development.lua", image = "app/modules/developer/gfx/settings.png", bgtask = true, offline = true}
 }
 
 local function openPage(opts)
