9fd75374027a1056637caec207e4cdcf257a17bd
Rob Thomson <rob@marotori.com>
2026-02-16 11:26:29 +0000

    cleanup orphans

--- src/rfsuite/app/modules/tools/menu_hooks.lua
+++
@@ -1,8 +0,0 @@
---[[
-  Copyright (C) 2026 Rotorflight Project
-  GPLv3 -- https://www.gnu.org/licenses/gpl-3.0.en.html
-]] --
-
--- Menu definitions moved to app/modules/manifest.lua.
--- This file is kept as a compatibility stub.
-return {}
